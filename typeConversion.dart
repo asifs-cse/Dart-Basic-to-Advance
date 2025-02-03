762cef6243c4fb6d26e34e17be6c2cae627ccb92
Asif Shahriar <asifshahriar@Asifs-MacBook-Air.local>
2025-02-03 11:35:24 +0600

    type conversion

--- typeConversion.dart
+++ typeConversion.dart
@@ -4,4 +4,10 @@ void main(){
   print("After conversion is $intvalue");
 
   print("Type of intvalue is ${intvalue.runtimeType}");
+
+  //string to double
+  String phone = "123456";
+  print("type of phone is ${phone.runtimeType}");
+  double number = double.parse(phone);
+  print("After convert double $number");
 }
