569a95f514931faafd96cff23eea6348e69aeded
Asif Shahriar <asifshahriar@Asifs-MacBook-Air.local>
2025-02-03 10:46:26 +0600

    data type

---
+++ dataType.dart
@@ -0,0 +1,18 @@
+void main(){
+  //Declaring variables
+  int num1 = 100;
+  double num2 = 130.2;
+  num num3 = 50;
+  num num4 = 50.4;
+
+  //For sum
+  num sum = num1+num2+num3+num4;
+
+  //pringint Info
+  print("Num 1 is $num1");
+  print("Num 2 is $num2");
+  print("Num 3 is $num3");
+  print("Num 4 is $num4");
+  print("Sum is $sum");
+  
+}
